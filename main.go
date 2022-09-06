ed4ca5ec042cdb40eba667dfc85056fd92e23f73
Dassyareg <dassyareg@gmail.com>
2022-09-06 14:42:04 +0100

    Added a few parameters

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+// Number of times to eat the meal
+const feeding = 3
+
+var duration = struct {
+	think int
+	eat   int
+}{
+	think: 2,
+	eat:   3,
+}
+
+var sticks = map[int]string {
+	1: "free",
+	2: "free",
+	3: "free",
+	4: "free",
+	5: "free",
+}
+
+func main() {
+
+	// Set the table - Hypothetical
+	
+
+	philosopher := []string{"Socrates", "Locke", "Plato", "Krato", "Aristotle"}
+	
+
+}
